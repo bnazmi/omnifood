a90f6d407d2a73856fed41d657759e850c25a004
abd <6nazmi@gmailc.om>
2018-01-04 16:09:06 +0200

    Add Animation by using Abimate.css libary and waypoints.js when scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -60,5 +60,30 @@ $(document).ready(function() {
         }
       }
     });
-      
+
+    /* Animation */
+
+    $('.js--wp-1').waypoint(function (direction) {
+      $('.js--wp-1').addClass('animated feedIn')
+    }, {
+      offset: '50%'
+    })
+     
+    $('.js--wp-2').waypoint(function(direction){
+      $('.js--wp-2').addClass('animated feedUp')
+    },{
+      offset: '50%'
+    })
+
+    $('.js--wp-3').waypoint(function (direction) {
+      $('.js--wp-3').addClass('animated feedIn')
+    }, {
+      offset: '50%'
+    })
+
+    $('.js--wp-4').waypoint(function (direction) {
+      $('.js--wp-4').addClass('animated pulse')
+    }, {
+      offset: '30%'
+    })
 })
